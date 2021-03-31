f9134ba410388fb9a8f07e7e4f8d17976d3ae2cf
gaborszantonk <gaborszanto.nk@gmail.com>
2021-03-31 09:38:45 +0200

    értékelés és módosítás

--- piton/Magyar_Daniel_dolgozat.py
+++ piton/Magyar_Daniel_dolgozat.py
@@ -1,6 +1,18 @@
 print("Adj meg szavakat amikben van f én kicserélem ph-ra.")
 a = str(input("Adj meg karaktersorozatot:   "))
 b = "ph"
-for cv in range(len(a)):
-    print(a.replace("f", b))
-    break
+#for cv in range(len(a)):
+print(a.replace("f", b))
+  #  break
+print("------")
+print("Értékelés")
+print("Dani, ha metódust - beépített függvényt használsz, akkor nem kell ciklus.. - Nézd meg a kódodat...\n Ha ciklust használsz, akkor így kellene: ")
+sor:str =''
+a = input("Kezdjük. Add meg a karaktersorozatot!    :")
+for cv in range(len(a)):  #A beírt sorozat végéig ismétli , hogy....
+    if a[cv] == 'f':  # Minden egyes elemnél megvizsgálja, hogy egyenlő-é f-fel, 
+        sor += 'ph'  #s ha igen, beírja a sor nevű változóba, ahol tároljuk az a sorozat betűit...
+    else:
+        sor += a[cv]  # ha az elem nem f, akkor beleírja az elemet a sor változóba
+print(sor)
+print("A tiéd egyszerűbb, elegánsabb - csak kevésbé szólt a for és az if gyakorlásáról - a replace esetében nem kell minden elemen végigmenni...")
